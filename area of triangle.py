8fd93e385fc657c5621c1af280a2f721ca0ca1ba
anilaghimire <amiableella6@gmail.com>
2021-12-01 16:55:01 +0545

    practise

---
+++ area of triangle.py
@@ -0,0 +1,6 @@
+b = int(input ("enter the value of length in cm:" ))
+h= int(input ("enter the value of the height in cm:"))
+A=(b+h)//2
+
+print("the area of right angled triangle is {} cm\u00b2",format(A))
+
---
+++ sum of int.py
@@ -0,0 +1,5 @@
+#adding even integer of first possible integer
+listnum= ["2,4,6,8,10,12,14,16,18,20"]
+sumA= sum(listnum)
+print (sumA)
+
