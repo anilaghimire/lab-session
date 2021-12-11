42fb7ffdf5561556f21b9952b8031ba63c7684f6
anilaghimire <amiableella6@gmail.com>
2021-12-11 17:21:11 +0545

    lab

--- day 4/lab5.py
+++ day 4/lab5.py
@@ -1,3 +1,6 @@
+#A student will not be allowed to sit in exam if his/her attendence is less than 75%. input from user Number of classes held
+#Number of classes attended.And print percentage of class attended Is student is allowed to sit in exam or not.
+
 a=int(input("number of class held:"))
 b=int(input("number of classes attended:"))
 percentage= b/a*100
